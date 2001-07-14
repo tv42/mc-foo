5dcfd285a5719409ec37018364889f14168ec359
tv42 <tv42>
2001-07-14 17:30:05 +0000

    Unsuccesfull child exits cause EIO return, handle the exception as EOF.
    Move self.connected to be implicit, based on whether self.fd is None
    or not. Add __getattr__() to provide the attribute.
    
    setblocking() can be called when not connected, it will remember the
    state and do as asked when connection is started. We don't really have
    an fd until we're connected, as opposed to real sockets.
    
    Be more careful invalidating self.fd, and checking that it is set.

--- lib/socket_pty.py
+++ lib/socket_pty.py
@@ -1,24 +1,34 @@
 import os
+
+from errno import EIO
+
 class socket_pty:
     def __init__(self):
-	self.connected=0
 	self.fd=None
 	self.nonblock=0
 
-    def setblocking(self, bool=None):
-	if bool!=None:
-	    self.nonblock=bool
-	else:
-	    bool=self.nonblock
-	if self.fd!=None:
-	    import fcntl
-	    import FCNTL
-	    flags = fcntl.fcntl(self.fd, FCNTL.F_GETFL, 0)
-	    if bool:
-		flags = flags | FCNTL.O_NONBLOCK
-	    else:
-		flags = flags & ~FCNTL.O_NONBLOCK
-		fcntl.fcntl (self.fd, FCNTL.F_SETFL, flags)
+    def __getattr__(self, name):
+        if name=='connected':
+            return self.fd!=None
+        else:
+            raise AttributeError, name
+
+    def setblocking(self, bool):
+        self.nonblock=bool
+        if self.fd!=None:
+            self.refresh_nonblocking()
+
+    def refresh_nonblocking(self):
+        if self.fd==None:
+            raise 'fd still None'
+        import fcntl
+        import FCNTL
+        flags = fcntl.fcntl(self.fd, FCNTL.F_GETFL, 0)
+        if self.nonblock:
+            flags = flags | FCNTL.O_NONBLOCK
+        else:
+            flags = flags & ~FCNTL.O_NONBLOCK
+        fcntl.fcntl (self.fd, FCNTL.F_SETFL, flags)
 
     def setsockopt(self, *foo):
 	raise "not a socket"
@@ -34,7 +44,8 @@ class socket_pty:
 
     def connect(self, cmd):
 	import pty
-	self.close()
+        if self.fd!=None:
+            self.close()
 	pid, fd = pty.fork()
 	if pid==0:
 	    # child
@@ -42,21 +53,35 @@ class socket_pty:
 	else:
 	    # parent
 	    self.fd=fd
-	    self.setblocking()
-	    self.connected=1
+            self.refresh_nonblocking()
 
     def accept(self, addr):
 	raise "not a socket"
 
     def send(self, data):
+        if self.fd==None:
+            raise 'fd still None'
 	return os.write(self.fd, data)
 
     def recv(self, buffer_size):
-	return os.read(self.fd, buffer_size)
+        if self.fd==None:
+            raise 'fd still None'
+        try:
+            return os.read(self.fd, buffer_size)
+        except OSError, e:
+            if e==EIO:
+                self.fd=None
+                return ''
+            else:
+                raise
 
     def close(self):
-	if self.fd!=None:
-	    os.close(self.fd)
+        if self.fd==None:
+            raise 'fd still None'
+        os.close(self.fd)
+        self.fd=None
 
     def fileno(self):
+        if self.fd==None:
+            raise 'fd still None'
 	return self.fd
